7e64104809a5a6939e43b0397ccdd7601578a994
agres <agres.dominik@gmail.com>
2025-03-22 17:03:50 +0100

    Further code for extracting data from the gdpr files

--- src/gdpr_export.py
+++ src/gdpr_export.py
@@ -1,41 +1,83 @@
 import json
 import os
-from collections import defaultdict
 
+from database_handler import Database, Table
+
+# Define the absolute folder path to the folder containing the gdrp retrieved data
 folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'gdpr_data')
-
-data_dictionary_name = defaultdict(int)
-data_dictionary_artist = defaultdict(int)
-data_dictionary_album = defaultdict(int)
-data_dictionary_conn_country = defaultdict(int)
+# Define the db
+db = Database()
 
 
-for filename in os.listdir(folder_path):
+def read_gdrp_data() -> dict:
+    """
+    This function reads all .json files in the folder containing the gdpr data.
+    This data is then extracted into a dict and sorted by timestamp ascending.
 
-    if filename.endswith('.json'):
-        file_path = os.path.join(folder_path, filename)
+    :return: all_songs_played: A dict with an items field containing all songs played for the user
+    """
+    all_songs_played = {
+        'items': []
+    }
 
-        with open(file_path, 'r') as file:
-            data = json.load(file)
+    for filename in os.listdir(folder_path):
 
-            for entry in data:
-                try:
-                    track_id = entry['spotify_track_uri']
-                    name = entry['master_metadata_track_name']
-                    artist = entry['master_metadata_album_artist_name']
-                    album = entry['master_metadata_album_album_name']
-                    conn_country = entry['conn_country']
-                    data_dictionary_name[name] += 1
-                    data_dictionary_artist[artist] += 1
-                    data_dictionary_album[album] += 1
-                    data_dictionary_conn_country[conn_country] += 1
-                except Exception as e:
-                    print(f'Missing field: {e}')
+        if filename.endswith('.json'):
+            file_path = os.path.join(folder_path, filename)
 
-sorted_name_counts_name = sorted(data_dictionary_name.items(), key=lambda x: x[1])
-sorted_name_counts_artist = sorted(data_dictionary_artist.items(), key=lambda x: x[1])
-sorted_name_counts_album = sorted(data_dictionary_album.items(), key=lambda x: x[1])
-sorted_name_counts_conn_country = sorted(data_dictionary_conn_country.items(), key=lambda x: x[1])
+            with open(file_path, 'r') as file:
+                data = json.load(file)
 
-for name, count in sorted_name_counts_conn_country:
-    print(f"{name}: {count}")
+                for entry in data:
+                    # This removes all podcasts from the list
+                    if entry['spotify_track_uri'] is None:
+                        continue
+                    try:
+                        track = {
+                            'timestamp': entry['ts'],
+                            'id': extract_id(entry['spotify_track_uri']),
+                            'track_name': entry['master_metadata_track_name'],
+                            'artist_name': entry['master_metadata_album_artist_name'],
+                            'album_name': entry['master_metadata_album_album_name'],
+                            'conn_country': entry['conn_country'],
+                            'ms_played': entry['ms_played']
+                            }
+                        all_songs_played['items'].append(track)
+                    except Exception as e:
+                        print(f'Missing field: {e}')
+
+    all_songs_played['items'] = sorted(all_songs_played['items'], key=lambda x: x['timestamp'])
+    return all_songs_played
+
+
+def extract_id(spotify_id: str) -> str:
+    """
+    This function gets a id with extra details and extracts the id from it.
+
+    :param: id a string containing the id
+    :return: str the ID
+    """
+    prefix = "spotify:track:"
+    prefix_removed_id = spotify_id[len(prefix):]
+    print(prefix_removed_id)
+    return prefix_removed_id
+
+
+def populate_ids(all_songs_played: dict):
+    pass
+    # for entry in all_songs_played['items']:
+    #   track_id = entry['id']
+
+
+def insert_data_into_db(all_songs_played: dict):
+    """
+    This function takes a list of all played songs and inserts these into the database.
+
+    :param: all_songs_played list of all songs
+    """
+
+    for entry in all_songs_played['items']:
+        db.add_row(Table.RECENTLY_PLAYED, (entry['timestamp'], entry['id'], entry['']))
+
+
+read_gdrp_data()
