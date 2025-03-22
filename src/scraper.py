535ff36aed9a4b071e52ff9a2ea7d00f51faeb8e
agres <agres.dominik@gmail.com>
2025-03-22 17:01:00 +0100

    Moved the API usage wrappers into an own file, added a function for getting multiple track_ids at once, this still needs to be tested more

--- src/scraper.py
+++ src/scraper.py
@@ -1,13 +1,14 @@
-import os
-
-import requests
-
 from auth import authenticate, simple_authenticate
 from database_handler import Database, Table
+from spotify_api import (
+    get_album_information,
+    get_artist_information,
+    get_last_played_track,
+    get_track_information,
+)
 
-# Define path to folder containing the database
-database_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'spotify_scraped.db')
-db = Database(database_path)
+# Define DB
+db = Database()
 
 
 def scraping():
@@ -31,7 +32,7 @@ def _read_recently_played_page_and_add_to_db(bearer_token: str):
     """
     global db
 
-    last_played_track = _get_last_played_track(bearer_token=bearer_token)
+    last_played_track = get_last_played_track(bearer_token=bearer_token)
 
     for track in last_played_track['items']:
         track_id = track['track']['id']
@@ -39,81 +40,6 @@ def _read_recently_played_page_and_add_to_db(bearer_token: str):
         album_id = track['track']['album']['id']
         artist_id = track['track']['artists'][0]['id']
         db.add_row(Table.RECENTLY_PLAYED, (played_at, track_id, artist_id, album_id))
-
-
-def _get_last_played_track(url: str = "https://api.spotify.com/v1/me/player/recently-played?limit=50", bearer_token: str = "") -> dict:
-    """
-    This function returns the last played track based on the limit size
-
-    :param limit: str
-    :param bearer_token: str
-    :return: dict
-    """
-
-    header = {
-        'Authorization': f'Bearer {bearer_token}'
-    }
-
-    response = requests.get(url, headers=header)
-    response_json = response.json()
-    return response_json
-
-
-def _get_track_information(track_id: str, bearer_token: str) -> dict:
-    """
-    This function returns the track information based on the track id
-
-    :param track_id: str
-    :param bearer_token: str
-    :return: dict
-    """
-
-    url = f"https://api.spotify.com/v1/tracks/{track_id}"
-    header = {
-        'Authorization': f'Bearer {bearer_token}'
-    }
-
-    response = requests.get(url, headers=header)
-    response_json = response.json()
-    return response_json
-
-
-def _get_artist_information(artist_id: str, bearer_token: str) -> dict:
-    """
-    This function returns the artist information based on the artist id
-
-    :param artist_id: str
-    :param bearer_token: str
-    :return: dict
-    """
-
-    url = f"https://api.spotify.com/v1/artists/{artist_id}"
-    header = {
-        'Authorization': f'Bearer {bearer_token}'
-    }
-
-    response = requests.get(url, headers=header)
-    response_json = response.json()
-    return response_json
-
-
-def _get_album_information(album_id: str, bearer_token: str) -> dict:
-    """
-    This function returns the album information based on the album id
-
-    :param album_id: str
-    :param bearer_token: str
-    :return: dict
-    """
-
-    url = f"https://api.spotify.com/v1/albums/{album_id}"
-    header = {
-        'Authorization': f'Bearer {bearer_token}'
-    }
-
-    response = requests.get(url, headers=header)
-    response_json = response.json()
-    return response_json
 
 
 def _scrape_missing_infos():
@@ -128,14 +54,14 @@ def _scrape_missing_infos():
     all_track_ids_saved = db.read_all_rows(Table.TRACK_INFORMATION, 'track_id')
     all_track_ids_missing = list(set(all_track_ids_recently_played) - set(all_track_ids_saved))
     for track_id in all_track_ids_missing:
-        response = _get_track_information(track_id=track_id[0], bearer_token=bearer_token_simple)
+        response = get_track_information(track_id=track_id[0], bearer_token=bearer_token_simple)
         db.add_row(Table.TRACK_INFORMATION, (response['id'], response['name'], response['duration_ms'], response['explicit'], response['popularity']))
     # Album Info
     all_album_ids_recently_played = db.read_all_rows(Table.RECENTLY_PLAYED, 'album_id')
     all_album_ids_saved = db.read_all_rows(Table.ALBUM_INFORMATION, 'album_id')
     all_album_ids_missing = list(set(all_album_ids_recently_played) - set(all_album_ids_saved))
     for album_id in all_album_ids_missing:
-        response = _get_album_information(album_id=album_id[0], bearer_token=bearer_token_simple)
+        response = get_album_information(album_id=album_id[0], bearer_token=bearer_token_simple)
         try:
             release_year = response['release_date'][:4]
         except Exception:
@@ -146,7 +72,7 @@ def _scrape_missing_infos():
     all_artist_ids_saved = db.read_all_rows(Table.ARTIST_INFORMATION, 'artist_id')
     all_artist_ids_missing = list(set(all_artist_ids_recently_played) - set(all_artist_ids_saved))
     for artist_id in all_artist_ids_missing:
-        response = _get_artist_information(artist_id=artist_id[0], bearer_token=bearer_token_simple)
+        response = get_artist_information(artist_id=artist_id[0], bearer_token=bearer_token_simple)
         try:
             genre = response['genres'][0]
         except IndexError:
