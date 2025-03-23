83d721f5dd7b81247eec19f63f926d83acb05e1f
agres <agres.dominik@gmail.com>
2025-03-23 15:33:50 +0100

    Added some small text for clean interface

--- src/runtime.py
+++ src/runtime.py
@@ -5,11 +5,13 @@ from gdpr_export import export_gdpr_data
 from scraper import scrape_missing_infos, scraping
 
 # Initialize the parser
-parser = argparse.ArgumentParser(description="Example with optional arguments")
+parser = argparse.ArgumentParser(description="A python script written in Python3.13 which continuously checks what spotify songs "
+                                             "the user is listening to and logging these in a local database. \n"
+                                             "The Script also has a export function where it can read out the gdpr data exported by the user.")
 
 # Add optional arguments
-parser.add_argument('--verbose', action='store_true', help="Enable verbose output")
-parser.add_argument('--export', action='store_true', help="Export the gdpr data from spotify if not done already")
+parser.add_argument('--verbose', '-v', action='store_true', help="Enable verbose output")
+parser.add_argument('--export', '-e', action='store_true', help="Export the gdpr data from spotify if not done already")
 
 # Parse the arguments
 args = parser.parse_args()
